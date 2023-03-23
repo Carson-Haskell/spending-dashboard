0e58ed2078a1a2a6f662f7a51eb8681bc3a5eb54
Carson Haskell <carson.haskell@gmail.com>
2023-03-23 16:36:55 -0700

    add expense item and date component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
-  return <h1>Project Start</h1>
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450.33,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <>
+      {expenses.map((expense) => {
+        const { id, title, amount, date } = expense;
+        return (
+          <ExpenseItem
+            key={id}
+            title={title}
+            amount={amount}
+            date={date}
+          />
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
